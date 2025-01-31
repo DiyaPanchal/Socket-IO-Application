7f6405914f6dc0265a16233ad15a965897c0f5f0
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2025-01-31 15:36:35 +0530

    socket functionality

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ const app = express();
 const PORT  = process.env.PORT || 3002;
 const server = createServer(app);
 
-app.listen(PORT, () => {
+app.use(express.static("public"));  
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("set username", (username) => {
+    socket.username = username;
+    console.log(`Username set: ${username}`);
+  });
+
+  socket.on("chat message", (msg) => {
+    const sender = socket.username || "Anonymous";
+    console.log(`Message from ${sender}: ${msg}`); 
+    io.emit("chat message", { sender, msg });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
