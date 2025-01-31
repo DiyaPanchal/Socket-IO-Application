8aab144d393f4ec8a51257b5de79e5fda9f146b8
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2025-01-31 15:15:26 +0530

    created http server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import "dotenv/config";
+import { createServer } from "http";
 
 const app = express();
 const PORT  = process.env.PORT || 3002;
+const server = createServer(app);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
